494fb81e8eb4686a6d708251e86e93748e9bc7c6
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-14 10:12:52 -0300

    criando o primeiro componente

---
+++ huntweb/src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//() ao invés de {} porque ele fará um retorno automaticamente
+
+//statesless component = criar a partir de uma função
+
+const Header = () => (
+    <header id='main-header'>JSHunt</header>
+);
+
+export default Header;
---
+++ huntweb/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //só vai usar uma unica fez para renderizar o primeiro componente
+import App from './App'; //
+import './style.css'; //
+
+ReactDOM.render(<App />, document.getElementById('root'));
