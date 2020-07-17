ecff192d0909ebaaf373b3b25e51217baf75a922
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-17 13:44:50 -0300

    adicionando rota para ir e voltar de página

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -5,19 +5,44 @@ import './style.css';
 export default class Main extends Component {
   state = {
     products: [],
+    productsInfo: [],
+    page: 1,
   };
 
   componentDidMount() {
     this.loadProducts()
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products')
-    this.setState({ products: response.data.docs })
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`)
+
+    const { docs, ...productsInfo } = response.data
+    this.setState({ products: docs, productsInfo, page })
+  }
+
+
+  nextPage = () => {
+    const { page, productsInfo } = this.state;
+
+    if (page === productsInfo.pages) return;
+
+    const pageNumber = page + 1
+
+    this.loadProducts(pageNumber)
+  }
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1
+
+    this.loadProducts(pageNumber)
   }
 
   render() {
-    const { products } = this.state
+    const { products, page, productsInfo } = this.state
     return (
       <div className='product-list'>
         {products.map(product => (
@@ -29,8 +54,8 @@ export default class Main extends Component {
           </article>
         ))}
         <div className='actions'>
-          <button>Anterior</button>
-          <button>Próxima</button>
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productsInfo.page} onClick={this.nextPage}>Próxima</button>
         </div>
       </div>
     )
